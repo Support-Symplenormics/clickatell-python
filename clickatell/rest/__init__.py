feeebef9e96605914c1ae85df47dadce22fd3d62
stephen.leibbrandt <stephen.leibbrandt@clickatell.com>
2017-10-03 16:52:55 +0200

    changed the clickatell rest to platform

--- clickatell/rest/__init__.py
+++ clickatell/rest/__init__.py
@@ -6,13 +6,13 @@ class Rest(Transport):
     Provides access to the Clickatell REST API
     """
 
-    def __init__(self, token):
+    def __init__(self, apiKey):
         """
-        Construct a new API instance with the auth token of the API
+        Construct a new API instance with the auth key of the API
 
-        :param str token: The auth token
+        :param str apiKey: The auth key
         """
-        self.token = token
+        self.apiKey = apiKey
         Transport.__init__(self)
 
     def request(self, action, data={}, headers={}, method='GET'):
@@ -20,9 +20,8 @@ class Rest(Transport):
         Append the REST headers to every request
         """
         headers = {
-            "Authorization": "Bearer " + self.token,
+            "Authorization": self.apiKey,
             "Content-Type": "application/json",
-            "X-Version": "1",
             "Accept": "application/json"
         }
 
@@ -36,8 +35,8 @@ class Rest(Transport):
         """
         to = to if isinstance(to, list) else [to]
         to = [str(num) for num in to]
-        data = {'to': to, 'text': message}
-        data = self.merge(data, {'callback': 7, 'mo': 1}, extra)
+        data = {'to': to, 'content': message}
+        data = self.merge(data, extra)
 
         content = self.parseRest(self.request('rest/message', data, {}, 'POST'));
         result = []
@@ -53,53 +52,3 @@ class Rest(Transport):
             });
 
         return result
-
-    def getBalance(self):
-        """
-        See parent method for documentation
-        """
-        content = self.parseRest(self.request('rest/account/balance'));
-        return {'balance': float(content['balance'])}
-
-    def stopMessage(self, apiMsgId):
-        """
-        See parent method for documentation
-        """
-        content =  self.parseRest(self.request('rest/message/' + apiMsgId, {}, {}, 'DELETE'))
-
-        return {
-            'id': content['apiMessageId'].encode('utf-8'),
-            'status': content['messageStatus'].encode('utf-8'),
-            'description': self.getStatus(content['messageStatus'])
-        }
-
-    def queryMessage(self, apiMsgId):
-        """
-        See parent method for documentation
-        """
-        return self.getMessageCharge(apiMsgId)
-
-    def getMessageCharge(self, apiMsgId):
-        """
-        See parent method for documentation
-        """
-        content = self.parseRest(self.request('rest/message/' + apiMsgId))
-
-        return {
-            'id': apiMsgId,
-            'status': content['messageStatus'].encode('utf-8'),
-            'description': self.getStatus(content['messageStatus']),
-            'charge': float(content['charge'])
-        }
-
-    def routeCoverage(self, msisdn):
-        """
-        If the route coverage lookup encounters an error, we will treat it as "not covered".
-        """
-        content = self.parseRest(self.request('rest/coverage/' + str(msisdn)))
-
-        return {
-            'routable': content['routable'],
-            'destination': content['destination'].encode('utf-8'),
-            'charge': float(content['minimumCharge'])
-        }
