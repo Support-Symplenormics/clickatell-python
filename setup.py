6f3d2bed98d529e6b673bd4930cfa640da40939a
stephen.leibbrandt <stephen.leibbrandt@clickatell.com>
2019-04-18 13:33:45 +0200

    updated  license, code for python3 and setup script for pypi

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="clickatell-platform",
-    version="2.0.0",
-    author="Chris Brand, Stephen Leibbrandt",
+    version="2.0.1",
+    author="Chris Brand, Stephen Leibbrandt, Renier Minne",
     author_email="support@clickatell.com",
-    keywords=["clickatell","sms"],
-    packages=find_packages(),
+    keywords=["clickatell","sms","platform"],
+    packages=setuptools.find_packages(),
     include_package_data=True,
     url="https://github.com/clickatell/clickatell-python",
-    license="LICENSE",
     description="Library for interacting with the Clickatell Platform SMS Gateway",
     long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="LICENSE",
     install_requires=[
         "httplib2",
-    ]
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
